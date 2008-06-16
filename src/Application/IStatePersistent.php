dce141d807dd547a7d1e80e391e094a6a8c2ae06
David Grudl <david@grudl.com>
2008-06-16 05:05:32 +0000

    - added Nette::Application - added examples

---
+++ src/Application/IStatePersistent.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Nette Framework
+ *
+ * Copyright (c) 2004, 2008 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license" that is bundled
+ * with this package in the file license.txt.
+ *
+ * For more information please see http://nettephp.com/
+ *
+ * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @license    http://nettephp.com/license  Nette license
+ * @link       http://nettephp.com/
+ * @category   Nette
+ * @package    Nette::Application
+ */
+
+/*namespace Nette::Application;*/
+
+
+
+/**
+ *
+ *
+ * @author     David Grudl
+ * @copyright  Copyright (c) 2004, 2008 David Grudl
+ * @package    Nette::Application
+ * @version    $Revision$ $Date$
+ */
+interface IStatePersistent
+{
+
+	/**
+	 * Loads state informations.
+	 * @param  array
+	 * @return void
+	 */
+	function loadState(array $params);
+
+	/**
+	 * Saves state informations for next request.
+	 * @param  array
+	 * @return void
+	 */
+	function saveState(array & $params);
+
+}
