8ec7a76d9a9e9623b1a09985e6d1180b5a5545d9
David Grudl <david@grudl.com>
2015-01-27 15:32:53 +0100

    TemplateFactory: nette/forms is optional

--- src/Bridges/ApplicationLatte/TemplateFactory.php
+++ src/Bridges/ApplicationLatte/TemplateFactory.php
@@ -70,7 +70,9 @@ class TemplateFactory extends Nette\Object implements UI\ITemplateFactory
 			$latte->getParser()->shortNoEscape = TRUE;
 			$latte->getCompiler()->addMacro('cache', new Nette\Bridges\CacheLatte\CacheMacro($latte->getCompiler()));
 			UIMacros::install($latte->getCompiler());
-			Nette\Bridges\FormsLatte\FormMacros::install($latte->getCompiler());
+			if (class_exists('Nette\Bridges\FormsLatte\FormMacros')) {
+				Nette\Bridges\FormsLatte\FormMacros::install($latte->getCompiler());
+			}
 			if ($control) {
 				$control->templatePrepareFilters($template);
 			}
