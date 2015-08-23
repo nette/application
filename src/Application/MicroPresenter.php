c22340f82e3205dba841c8a655e47b07ab170935
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-08-23 21:14:00 +0200

    fixed using undefined classes in typehints

--- src/Application/MicroPresenter.php
+++ src/Application/MicroPresenter.php
@@ -17,7 +17,7 @@ use Latte;
 /**
  * Micro presenter.
  *
- * @property-read Nette\Application\IRequest $request
+ * @property-read Nette\Application\Request $request
  */
 class MicroPresenter extends Nette\Object implements Application\IPresenter
 {
@@ -157,7 +157,7 @@ class MicroPresenter extends Nette\Object implements Application\IPresenter
 
 
 	/**
-	 * @return Nette\Application\IRequest
+	 * @return Nette\Application\Request
 	 */
 	public function getRequest()
 	{
