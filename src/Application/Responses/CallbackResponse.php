35b4cfe213c7ca337ccde98108e16aaec5cbbfa9
David Grudl <david@grudl.com>
2015-09-30 01:13:31 +0200

    added CallbackResponse

---
+++ src/Application/Responses/CallbackResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework (http://nette.org)
+ * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+ */
+
+namespace Nette\Application\Responses;
+
+use Nette;
+
+
+/**
+ * Callback response.
+ */
+class CallbackResponse extends Nette\Object implements Nette\Application\IResponse
+{
+	/** @var callable */
+	private $callback;
+
+
+	/**
+	 * @param  callable  function (Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse)
+	 */
+	public function __construct(callable $callback)
+	{
+		$this->callback = $callback;
+	}
+
+
+	/**
+	 * Sends response to output.
+	 * @return void
+	 */
+	public function send(Nette\Http\IRequest $httpRequest, Nette\Http\IResponse $httpResponse)
+	{
+		call_user_func($this->callback, $httpRequest, $httpResponse);
+	}
+
+}
