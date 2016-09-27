95433f75bf33dbbd779ef331161ddf1e38325bed
David Grudl <david@grudl.com>
2016-09-27 11:49:29 +0200

    tests: fixed compatibility with nette/http

--- tests/Routers/Route.php
+++ tests/Routers/Route.php
@@ -7,9 +7,10 @@
 use Tester\Assert;
 
 
-function testRouteIn(Nette\Application\IRouter $route, $url, $expectedPresenter = NULL, $expectedParams = NULL, $expectedUrl = NULL)
+function testRouteIn(Nette\Application\IRouter $route, $url, $expectedPresenter = NULL, $expectedParams = [], $expectedUrl = NULL)
 {
 	$url = new Nette\Http\UrlScript("http://example.com$url");
+	$url->setScriptPath('/');
 	$url->appendQuery([
 		'test' => 'testvalue',
 		'presenter' => 'querypresenter',
