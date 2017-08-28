359b26becebb7d9adfe9cd1be36ea0f6ebf5dd6e
Vlasta Vesely <vlastimil.vesely7@email.cz>
2017-08-28 11:06:09 +0200

    typo (#186)

--- src/Application/UI/ComponentReflection.php
+++ src/Application/UI/ComponentReflection.php
@@ -248,7 +248,7 @@ class ComponentReflection extends \ReflectionClass
 
 
 	/**
-	 * @return [string, bool]
+	 * @return array [string|null, bool]
 	 */
 	public static function getParameterType(\ReflectionParameter $param)
 	{
