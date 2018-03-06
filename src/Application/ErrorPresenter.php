8209936581af04833ff426081bf2edfe1e75bedf
David Grudl <david@grudl.com>
2018-03-06 11:08:21 +0100

    ErrorPresenter: renders error.phtml only in HTML

--- src/Application/ErrorPresenter.php
+++ src/Application/ErrorPresenter.php
@@ -11,6 +11,7 @@ namespace NetteModule;
 
 use Nette;
 use Nette\Application;
+use Nette\Http;
 use Tracy\ILogger;
 
 
@@ -42,8 +43,10 @@ final class ErrorPresenter implements Application\IPresenter
 				$this->logger->log($e, ILogger::EXCEPTION);
 			}
 		}
-		return new Application\Responses\CallbackResponse(function () use ($code) {
-			require __DIR__ . '/templates/error.phtml';
+		return new Application\Responses\CallbackResponse(function (Http\IRequest $httpRequest, Http\IResponse $httpResponse) use ($code) {
+			if (preg_match('#^text/html(?:;|$)#', $httpResponse->getHeader('Content-Type'))) {
+				require __DIR__ . '/templates/error.phtml';
+			}
 		});
 	}
 }
