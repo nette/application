64fcb18b23331b0f991c4d55bd9408b0b5cf4191
David Grudl <david@grudl.com>
2018-10-25 22:43:27 +0200

    Presenter: added support for typehint 'iterable' [Closes #203]

--- src/Application/UI/ComponentReflection.php
+++ src/Application/UI/ComponentReflection.php
@@ -179,7 +179,7 @@ class ComponentReflection extends \ReflectionClass
 				$res[$i] = $param->getDefaultValue();
 			} elseif ($type === 'NULL' || $param->allowsNull()) {
 				$res[$i] = null;
-			} elseif ($type === 'array') {
+			} elseif ($type === 'array' || $type === 'iterable') {
 				$res[$i] = [];
 			} else {
 				throw new BadRequestException(sprintf(
@@ -210,7 +210,7 @@ class ComponentReflection extends \ReflectionClass
 		} elseif ($type === 'NULL') { // means 'not array'
 			return !is_array($val);
 
-		} elseif ($type === 'array') {
+		} elseif ($type === 'array' || $type === 'iterable') {
 			return is_array($val);
 
 		} elseif (!is_scalar($val)) { // array, resource, null, etc.
--- src/Application/UI/Presenter.php
+++ src/Application/UI/Presenter.php
@@ -1027,7 +1027,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 			}
 
 			if (!isset($args[$name])) {
-				if (!$param->isDefaultValueAvailable() && !$param->allowsNull() && $type !== 'NULL' && $type !== 'array') {
+				if (!$param->isDefaultValueAvailable() && !$param->allowsNull() && $type !== 'NULL' && $type !== 'array' && $type !== 'iterable') {
 					$missing[] = $param;
 					unset($args[$name]);
 				}
