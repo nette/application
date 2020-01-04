24a69cff4bfc24f7c719c99e7d0bb79212148389
David Grudl <david@grudl.com>
2020-01-04 23:53:37 +0100

    Control, Presenter::createTemplate() tries to create custom Template according to naming convention

--- src/Application/UI/Presenter.php
+++ src/Application/UI/Presenter.php
@@ -557,7 +557,7 @@ abstract class Presenter extends Control implements Application\IPresenter
 
 	protected function createTemplate(): ITemplate
 	{
-		return $this->getTemplateFactory()->createTemplate($this);
+		return $this->getTemplateFactory()->createTemplate($this, $this->formatTemplateClass());
 	}
 
 
