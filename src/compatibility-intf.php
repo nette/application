d2233b176e5a093873baf714c5c9035d431f2338
David Grudl <david@grudl.com>
2021-03-03 22:14:57 +0100

    fixed compatibility with Symfony DebugClassLoader

--- src/compatibility-intf.php
+++ src/compatibility-intf.php
@@ -14,13 +14,16 @@ if (false) {
 	interface IRouter extends \Nette\Routing\Router
 	{
 	}
+} elseif (!interface_exists(IRouter::class)) {
+	class_alias(\Nette\Routing\Router::class, IRouter::class);
+}
 
+if (false) {
 	/** @deprecated use Nette\Application\Response */
 	interface IResponse extends Response
 	{
 	}
-} elseif (!interface_exists(IRouter::class)) {
-	class_alias(\Nette\Routing\Router::class, IRouter::class);
+} elseif (!interface_exists(IResponse::class)) {
 	class_alias(Response::class, IResponse::class);
 }
 
@@ -31,31 +34,43 @@ if (false) {
 	interface IRenderable extends Renderable
 	{
 	}
+} elseif (!interface_exists(IRenderable::class)) {
+	class_alias(Renderable::class, IRenderable::class);
+}
 
+if (false) {
 	/** @deprecated use Nette\Application\UI\SignalReceiver */
 	interface ISignalReceiver extends SignalReceiver
 	{
 	}
+} elseif (!interface_exists(ISignalReceiver::class)) {
+	class_alias(SignalReceiver::class, ISignalReceiver::class);
+}
 
+if (false) {
 	/** @deprecated use Nette\Application\UI\StatePersistent */
 	interface IStatePersistent extends StatePersistent
 	{
 	}
+} elseif (!interface_exists(IStatePersistent::class)) {
+	class_alias(StatePersistent::class, IStatePersistent::class);
+}
 
+if (false) {
 	/** @deprecated use Nette\Application\UI\Template */
 	interface ITemplate extends Template
 	{
 	}
+} elseif (!interface_exists(ITemplate::class)) {
+	class_alias(Template::class, ITemplate::class);
+}
 
+if (false) {
 	/** @deprecated use Nette\Application\UI\TemplateFactory */
 	interface ITemplateFactory extends TemplateFactory
 	{
 	}
-} elseif (!interface_exists(IRenderable::class)) {
-	class_alias(Renderable::class, IRenderable::class);
-	class_alias(SignalReceiver::class, ISignalReceiver::class);
-	class_alias(StatePersistent::class, IStatePersistent::class);
-	class_alias(Template::class, ITemplate::class);
+} elseif (!interface_exists(ITemplateFactory::class)) {
 	class_alias(TemplateFactory::class, ITemplateFactory::class);
 }
 
