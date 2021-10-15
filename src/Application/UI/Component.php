1710100574c0f9388938d08bb7bc32a690108256
David Grudl <david@grudl.com>
2021-10-15 18:14:52 +0200

    removed compatibility for old class names
    - inteface Nette\Application\IRouter replaced by Nette\Routing\Router

--- src/Application/UI/Component.php
+++ src/Application/UI/Component.php
@@ -337,6 +337,3 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 		throw new Nette\Application\BadRequestException($message, $httpCode);
 	}
 }
-
-
-class_exists(PresenterComponent::class);
