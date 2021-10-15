723c85555615c46dfe9568db3f1db30b8fe47bfc
David Grudl <david@grudl.com>
2021-10-15 18:14:52 +0200

    removed compatibility for old class names
    - inteface Nette\Application\IRouter replaced by Nette\Routing\Router

--- src/Application/Routers/RouteList.php
+++ src/Application/Routers/RouteList.php
@@ -11,7 +11,7 @@ namespace Nette\Application\Routers;
 
 use JetBrains\PhpStorm\Language;
 use Nette;
-use function count, interface_exists, is_int, is_string, strlen, strncmp, substr;
+use function count, is_int, is_string, strlen, strncmp, substr;
 
 
 /**
@@ -143,6 +143,3 @@ class RouteList extends Nette\Routing\RouteList implements Nette\Routing\Router,
 		$this->modify($index, null);
 	}
 }
-
-
-interface_exists(Nette\Application\IRouter::class);
