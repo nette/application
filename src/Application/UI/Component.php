b89b3d6a9297073938f61f6a8f7536f7501a3a36
David Grudl <david@grudl.com>
2021-10-20 01:51:59 +0200

    Revert "Component::link() & etc uses variadic parameter" [Closes #298]
    This reverts commit a987d588d78a1cb8535f097030eaa557ce17dc7d.

--- src/Application/UI/Component.php
+++ src/Application/UI/Component.php
@@ -245,15 +245,15 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	/**
 	 * Generates URL to presenter, action or signal.
 	 * @param  string   $destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
-	 * @param  mixed  ...$args
+	 * @param  array|mixed  $args
 	 * @throws InvalidLinkException
 	 */
-	public function link(string $destination, ...$args): string
+	public function link(string $destination, $args = []): string
 	{
 		try {
-			$args = count($args) === 1 && is_array($args[0] ?? null)
-				? $args[0]
-				: $args;
+			$args = func_num_args() < 3 && is_array($args)
+				? $args
+				: array_slice(func_get_args(), 1);
 			return $this->getPresenter()->createRequest($this, $destination, $args, 'link');
 
 		} catch (InvalidLinkException $e) {
@@ -265,13 +265,13 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	/**
 	 * Returns destination as Link object.
 	 * @param  string   $destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
-	 * @param  mixed  ...$args
+	 * @param  array|mixed  $args
 	 */
-	public function lazyLink(string $destination, ...$args): Link
+	public function lazyLink(string $destination, $args = []): Link
 	{
-		$args = count($args) === 1 && is_array($args[0] ?? null)
-			? $args[0]
-			: $args;
+		$args = func_num_args() < 3 && is_array($args)
+			? $args
+			: array_slice(func_get_args(), 1);
 		return new Link($this, $destination, $args);
 	}
 
@@ -279,15 +279,15 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	/**
 	 * Determines whether it links to the current page.
 	 * @param  string   $destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
-	 * @param  mixed  ...$args
+	 * @param  array|mixed  $args
 	 * @throws InvalidLinkException
 	 */
-	public function isLinkCurrent(string $destination = null, ...$args): bool
+	public function isLinkCurrent(string $destination = null, $args = []): bool
 	{
 		if ($destination !== null) {
-			$args = count($args) === 1 && is_array($args[0] ?? null)
-				? $args[0]
-				: $args;
+			$args = func_num_args() < 3 && is_array($args)
+				? $args
+				: array_slice(func_get_args(), 1);
 			$this->getPresenter()->createRequest($this, $destination, $args, 'test');
 		}
 		return $this->getPresenter()->getLastCreatedRequestFlag('current');
@@ -297,14 +297,14 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	/**
 	 * Redirect to another presenter, action or signal.
 	 * @param  string   $destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
-	 * @param  mixed  ...$args
+	 * @param  array|mixed  $args
 	 * @throws Nette\Application\AbortException
 	 */
-	public function redirect(string $destination, ...$args): void
+	public function redirect(string $destination, $args = []): void
 	{
-		$args = count($args) === 1 && is_array($args[0] ?? null)
-			? $args[0]
-			: $args;
+		$args = func_num_args() < 3 && is_array($args)
+			? $args
+			: array_slice(func_get_args(), 1);
 		$presenter = $this->getPresenter();
 		$presenter->redirectUrl($presenter->createRequest($this, $destination, $args, 'redirect'));
 	}
@@ -313,14 +313,14 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	/**
 	 * Permanently redirects to presenter, action or signal.
 	 * @param  string   $destination in format "[//] [[[module:]presenter:]action | signal! | this] [#fragment]"
-	 * @param  mixed  ...$args
+	 * @param  array|mixed  $args
 	 * @throws Nette\Application\AbortException
 	 */
-	public function redirectPermanent(string $destination, ...$args): void
+	public function redirectPermanent(string $destination, $args = []): void
 	{
-		$args = count($args) === 1 && is_array($args[0] ?? null)
-			? $args[0]
-			: $args;
+		$args = func_num_args() < 3 && is_array($args)
+			? $args
+			: array_slice(func_get_args(), 1);
 		$presenter = $this->getPresenter();
 		$presenter->redirectUrl(
 			$presenter->createRequest($this, $destination, $args, 'redirect'),
