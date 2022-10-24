3ff5168cf0c65966ae9ca35069885c7681bc4c2a
David Grudl <david@grudl.com>
2022-10-24 19:12:55 +0200

    Revert "UI\PresenterComponent: removed references created by loadState() for persistent parameters. [Closes nette/nette#703][Closes nette/nette#703][Closes #69]" (possible BC break)
    This reverts commit cda17f460d020b0f042364d4e140742022a7e94d.
    
    See https://forum.nette.org/cs/35528-stejne-pojmenovany-parametr-akce-presenteru-a-persistentni-odlisne-chovani-v-nette-2-0-oproti-aktualnimu#p221742
    
    BC break: Property must be nullable, ie: #[Persistent] public ?int $foo

--- src/Application/UI/Component.php
+++ src/Application/UI/Component.php
@@ -160,9 +160,9 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 					));
 				}
 
-				$this->$name = $params[$name];
+				$this->$name = &$params[$name];
 			} else {
-				$params[$name] = $this->$name ?? null;
+				$params[$name] = &$this->$name;
 			}
 		}
 
@@ -196,7 +196,7 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	 */
 	final public function getParameters(): array
 	{
-		return $this->params;
+		return array_map(fn($item) => $item, $this->params);
 	}
 
 
