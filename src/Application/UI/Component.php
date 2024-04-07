5a5ddf3c74b878e02c37225f01b6d140048d7345
David Grudl <david@grudl.com>
2024-04-07 04:33:34 +0200

    Component: only UI components can be added to presenter/component (BC break) WIP

--- src/Application/UI/Component.php
+++ src/Application/UI/Component.php
@@ -71,18 +71,26 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	}
 
 
+	public function addComponent(
+		Nette\ComponentModel\IComponent $component,
+		?string $name,
+		?string $insertBefore = null,
+	): static
+	{
+		if (!$component instanceof SignalReceiver && !$component instanceof StatePersistent) {
+			throw new Nette\InvalidStateException("Component '$name' of type " . get_debug_type($component) . ' is not intended to be used in the Presenter.');
+		}
+
+		return parent::addComponent($component, $name, $insertBefore = null);
+	}
+
+
 	protected function createComponent(string $name): ?Nette\ComponentModel\IComponent
 	{
 		if (method_exists($this, $method = 'createComponent' . $name)) {
 			$this->checkRequirements(new \ReflectionMethod($this, $method));
 		}
-		$res = parent::createComponent($name);
-		if ($res && !$res instanceof SignalReceiver && !$res instanceof StatePersistent) {
-			$type = $res::class;
-			trigger_error("It seems that component '$name' of type $type is not intended to be used in the Presenter.");
-		}
-
-		return $res;
+		return parent::createComponent($name);
 	}
 
 
