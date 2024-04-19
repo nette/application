db460f64434be8f16d7b4138ac463c7e2c27617d
David Grudl <david@grudl.com>
2024-04-19 18:59:59 +0200

    @annotations are deprecated (BC break)

--- src/Application/UI/MethodReflection.php
+++ src/Application/UI/MethodReflection.php
@@ -18,6 +18,7 @@ final class MethodReflection extends \ReflectionMethod
 	#[\Deprecated]
 	public function hasAnnotation(string $name): bool
 	{
+		trigger_error(__METHOD__ . '() is deprecated', E_USER_DEPRECATED);
 		return (bool) ComponentReflection::parseAnnotation($this, $name);
 	}
 
@@ -25,6 +26,7 @@ final class MethodReflection extends \ReflectionMethod
 	#[\Deprecated]
 	public function getAnnotation(string $name): mixed
 	{
+		trigger_error(__METHOD__ . '() is deprecated', E_USER_DEPRECATED);
 		$res = ComponentReflection::parseAnnotation($this, $name);
 		return $res ? end($res) : null;
 	}
