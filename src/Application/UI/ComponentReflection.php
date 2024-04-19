b144545843ed330c090d333a7c7bc434a0f6148c
David Grudl <david@grudl.com>
2024-04-19 18:59:59 +0200

    @annotations are deprecated (BC break)

--- src/Application/UI/ComponentReflection.php
+++ src/Application/UI/ComponentReflection.php
@@ -28,7 +28,7 @@ final class ComponentReflection extends \ReflectionClass
 
 
 	/**
-	 * Returns array of class properties that are public and have attribute #[Persistent] or #[Parameter] or annotation @persistent.
+	 * Returns array of class properties that are public and have attribute #[Persistent] or #[Parameter].
 	 * @return array<string, array{def: mixed, type: string, since: ?string}>
 	 */
 	public function getParameters(): array
@@ -75,7 +75,7 @@ final class ComponentReflection extends \ReflectionClass
 
 
 	/**
-	 * Returns array of persistent properties. They are public and have attribute #[Persistent] or annotation @persistent.
+	 * Returns array of persistent properties. They are public and have attribute #[Persistent].
 	 * @return array<string, array{def: mixed, type: string, since: string}>
 	 */
 	public function getPersistentParams(): array
@@ -150,6 +150,7 @@ final class ComponentReflection extends \ReflectionClass
 
 	/**
 	 * Returns an annotation value.
+	 * @deprecated
 	 */
 	public static function parseAnnotation(\Reflector $ref, string $name): ?array
 	{
@@ -167,12 +168,20 @@ final class ComponentReflection extends \ReflectionClass
 			}
 		}
 
+		$alt = match ($name) {
+			'persistent' => '#[Nette\Application\Attributes\Persistent]',
+			'deprecated' => '#[Nette\Application\Attributes\Deprecated]',
+			'crossOrigin' => '#[Nette\Application\Attributes\Request(sameOrigin: false)]',
+			default => 'alternative'
+		};
+		trigger_error("Annotation @$name is deprecated, use $alt (used in " . Nette\Utils\Reflection::toString($ref) . ')', E_USER_DEPRECATED);
 		return $res;
 	}
 
 
 	/**
 	 * Has class specified annotation?
+	 * @deprecated
 	 */
 	public function hasAnnotation(string $name): bool
 	{
@@ -182,6 +191,7 @@ final class ComponentReflection extends \ReflectionClass
 
 	/**
 	 * Returns an annotation value.
+	 * @deprecated
 	 */
 	public function getAnnotation(string $name): mixed
 	{
