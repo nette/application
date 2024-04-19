0098f564a3b7129c79abdca318ecc2de51bea8a4
David Grudl <david@grudl.com>
2024-04-19 19:01:35 +0200

    removed {templatePrint}

--- src/Bridges/ApplicationLatte/UIExtension.php
+++ src/Bridges/ApplicationLatte/UIExtension.php
@@ -78,7 +78,6 @@ final class UIExtension extends Latte\Extension
 			'plink' => Nodes\LinkNode::create(...),
 			'link' => Nodes\LinkNode::create(...),
 			'linkBase' => Nodes\LinkBaseNode::create(...),
-			'templatePrint' => Nodes\TemplatePrintNode::create(...),
 			'snippet' => Nodes\SnippetNode::create(...),
 			'snippetArea' => Nodes\SnippetAreaNode::create(...),
 			'layout' => $this->createExtendsNode(...),
