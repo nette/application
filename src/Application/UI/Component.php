70210a1a3ce98213906f7b86284d2ea31e5fca18
David Grudl <david@grudl.com>
2024-04-20 02:43:19 +0200

    Component: method checkRequirements() is called for createComponent<Name>() methods (BC break)

--- src/Application/UI/Component.php
+++ src/Application/UI/Component.php
@@ -70,7 +70,9 @@ abstract class Component extends Nette\ComponentModel\Container implements Signa
 	protected function createComponent(string $name): ?Nette\ComponentModel\IComponent
 	{
 		if (method_exists($this, $method = 'createComponent' . $name)) {
-			(new AccessPolicy(new \ReflectionMethod($this, $method)))->checkAccess($this);
+			$rm = new \ReflectionMethod($this, $method);
+			(new AccessPolicy($rm))->checkAccess($this);
+			$this->checkRequirements($rm);
 		}
 		$res = parent::createComponent($name);
 		if ($res && !$res instanceof SignalReceiver && !$res instanceof StatePersistent) {
